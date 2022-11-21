57bbe7ad7a0db653d4683246a2ed1affd881b86b
por <73821021+sakaratsooksang@users.noreply.github.com>
2022-11-21 16:08:56 +0700

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+myself = {
+  "name" : "Sakarat Sooksang",
+  "university" : "Chulalongkorn University",
+  "skills" : ["Spread Sheet","Python","SQL","R"]
+}
+print(f"Hello my name is {myself.name}! nice too see you :)"
