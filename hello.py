029c802790b65d51e8934ae8a094f862873d1d00
por <binnerland2542@gmail.com>
2022-11-23 12:22:30 +0700

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-myself = {
-  "name" : "Sakarat Sooksang",
-  "university" : "Chulalongkorn University",
-  "skills" : ["Spread Sheet","Python","SQL","R"]
-}
-print(f"Hello my name is {myself.name}! nice too see you :)"
